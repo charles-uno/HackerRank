f67a36a8180d65dae70fff00292be6280217d80d
Charles <charles.a.mceachern@gmail.com>
2018-04-17 22:22:41 -0500

    looks like that prime generator might pay off

---
+++ pe-007.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Charles McEachern
+2018-04-17
+https://projecteuler.net/problem=7
+"""
+
+import helpers
+
+# ######################################################################
+
+def main():
+    print( helpers.nth_prime(10001) )
+
+# ######################################################################
+
+if __name__ == '__main__':
+    main()
