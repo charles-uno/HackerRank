eb11dd0a0f876e9198465d0b079fa5dab3f6c5de
Charles <charles.a.mceachern@gmail.com>
2018-04-17 21:34:36 -0500

    some prime factorization

---
+++ pe-003.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"""
+Charles McEachern
+2018-04-17
+https://projecteuler.net/problem=3
+"""
+
+import math
+
+# ######################################################################
+
+def main():
+    print( max( factorize(600851475143) ) )
+
+# ----------------------------------------------------------------------
+
+def factorize(n):
+    # Don't even bother checking evens.
+    primes = []
+    i = 1
+    while n > 1:
+        i += 2
+        if any( i % p == 0 for p in primes ):
+            continue
+        # Looks like we found a prime!
+        primes.append(i)
+        # If it's a divisor, divide it out. This will let us hit our
+        # exit condition sooner.
+        while n % i == 0:
+            n //= i
+            yield i
+
+# ######################################################################
+
+if __name__ == '__main__':
+    main()
