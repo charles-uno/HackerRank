3d3cf51086b60eb2e1514689ea64d9450a5c1d23
Charles <charles.a.mceachern@gmail.com>
2018-04-20 18:03:43 -0500

    cleaned up factorization helpers

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -10,7 +10,7 @@ def scm(*args):
     # power of 3, and so on.
     power_by_factor = collections.defaultdict(int)
     for n in args:
-        for f, p in get_power_by_factor(n).items():
+        for f, p in factor_counts(n).items():
             power_by_factor[f] = max(power_by_factor[f], p)
     # Now return the product of all those maximal powers.
     tmp = 1
@@ -18,29 +18,47 @@ def scm(*args):
         tmp *= f**p
     return tmp
 
-# ----------------------------------------------------------------------
+# ######################################################################
 
-def get_power_by_factor(n):
-    factors = list( factorize(n) )
-    return { x:factors.count(x) for x in set(factors) }
-
-# ----------------------------------------------------------------------
+PRIMES = [2]
 
 def factorize(n):
-    primes = []
-    i = 1
+    # In most situations, it's faster to remember primes as we find
+    # them. Potential downside is, if we accumulate a ton of primes, it
+    # now takes longer to factor small numbers. But problems trpically
+    # have us worry about stuff in ascending order anyway.
+    global PRIMES
+    # Start by checking for prime factors we have encountered already.
+    for p in PRIMES:
+        while n % p == 0:
+            n //= p
+            yield p
+    # Now check for additional primes. Note that we stores primes in
+    # ascending order, so the last one is the largest.
+    i = PRIMES[-1]
     while n > 1:
         i += 1
-        if any( i % p == 0 for p in primes ):
+        if any( i % p == 0 for p in PRIMES ):
             continue
-        # Looks like we found a prime!
-        primes.append(i)
-        # Divide out divisors. Watch out for degeneracy.
+        # Looks like we found another prime!
+        PRIMES.append(i)
+        # Now check if it's a divisor.
         while n % i == 0:
             n //= i
             yield i
 
 # ----------------------------------------------------------------------
+
+def factor_counts(n):
+    """Factorize a number, then return a dictionary. Index is prime
+    factors, value is the power of that factor.
+    """
+    factors = list( factorize(n) )
+    return { x:factors.count(x) for x in set(factors) }
+
+# ######################################################################
+
+
 
 def nth_prime(n):
     primes = []
@@ -65,16 +83,19 @@ def primes_upto(n):
         if sieve[i]:
             yield i
 
-
-
-
-
-
-
-# ----------------------------------------------------------------------
+# ######################################################################
 
 def divisors(n):
-    for i in range(1, n//2):
+    """Find all divisors of a number by brute force."""
+    for i in range(1, n//2+1):
         if n % i == 0:
             yield i
     yield n
+
+# ----------------------------------------------------------------------
+
+def ndivisors(n):
+    """Factorize a number to figure out how many divisors it has,
+    without actually generating those divisors.
+    """
+    return np.prod( [ x+1 for x in factor_counts(n).values() ] )
--- pe-012.py
+++ pe-012.py
@@ -6,8 +6,6 @@ Charles McEachern
 https://projecteuler.net/problem=12
 """
 
-import numpy as np
-
 import helpers
 
 # ######################################################################
@@ -17,64 +15,11 @@ def main():
     while True:
         n += 1
         tri = n*(n+1)//2
-        # Right off the bat, we can break a triangular number into two
-        # smaller factors. This should make it quicker to factorize.
-        if n % 2 == 0:
-            factors = factorize(n//2) + factorize(n+1)
-        else:
-            factors = factorize(n) + factorize(n//2+1)
-        # If n contains 2 powers of 17, that's 3 options: 17^0, 17^1,
-        # 17^2. And all prime factors are independent, of course.
-        ndivisors = np.prod( [ x+1 for x in value_counts(factors).values() ] )
+        ndivisors = helpers.ndivisors(tri)
         if ndivisors > 500:
             print(n, ':', tri, ':', ndivisors)
             break
     return
-
-# ----------------------------------------------------------------------
-
-FACTORS = {}
-
-def factorize(n):
-    """We can save a bit of time by remembering factors."""
-    global FACTORS
-    if n not in FACTORS:
-        FACTORS[n] = list( factorize_many(n) )
-    return FACTORS[n]
-
-# ----------------------------------------------------------------------
-
-# Don't want to bother checking for empty lists.
-PRIMES = [2]
-
-def factorize_many(n):
-    """If we're factoring a ton of numbers -- or, really, anything in
-    ascending order -- it's faster to remember primes as we find them.
-    """
-    global PRIMES
-    # First, check primes that we have encountered in the past.
-    for p in PRIMES:
-        while n % p == 0:
-            n //= p
-            yield p
-    # Now check for additional primes. Note that we stores primes in
-    # ascending order, so the last one is the largest.
-    i = PRIMES[-1]
-    while n > 1:
-        i += 1
-        if any( i % p == 0 for p in PRIMES ):
-            continue
-        # Looks like we found another prime!
-        PRIMES.append(i)
-        # Now check if it's a divisor.
-        while n % i == 0:
-            n //= i
-            yield i
-
-# ----------------------------------------------------------------------
-
-def value_counts(seq):
-    return { x:seq.count(x) for x in set(seq) }
 
 # ######################################################################
 
