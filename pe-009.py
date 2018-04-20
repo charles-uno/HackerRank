17e1e039806e062101ddfeb985111b7d274a83cf
Charles <charles.a.mceachern@gmail.com>
2018-04-19 19:36:25 -0500

    11 is annoying, but not hard

--- pe-009.py
+++ pe-009.py
@@ -4,16 +4,6 @@
 Charles McEachern
 2018-04-18
 https://projecteuler.net/problem=9
-
-
-A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
-a**2 + b**2 = c**2
-
-For example, 3**2 + 4**2 = 9 + 16 = 25 = 5**2.
-
-There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find
-the product abc.
-
 """
 
 import helpers
